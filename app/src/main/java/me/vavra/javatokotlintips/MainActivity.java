25bfd97ac74f1f50ac59c2fe111c5323870bf89e
David Vavra <david@vavra.me>
2017-09-21 23:09:14 +0300

    Promo screen

--- app/src/main/java/me/vavra/javatokotlintips/MainActivity.java
+++ app/src/main/java/me/vavra/javatokotlintips/MainActivity.java
@@ -2,12 +2,23 @@ package me.vavra.javatokotlintips;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.vFab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNumber++;
+                ((TextView)findViewById(R.id.vNumber)).setText(String.valueOf(mNumber));
+            }
+        });
     }
 }
